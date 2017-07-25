fab80f352cc98d5b92f204dbb377877d83be00e3
GoldK11 <ssori113@gmail.com>
2017-07-25 17:41:58 +0900

    Review day1

---
+++ day1.py
@@ -0,0 +1,17 @@
+print('I want to calculate sphere, (set pi=3.1415)')
+
+r=int(input('Please enter radious of this sphere: '))
+pi=3.1415
+d=2*r
+c=round(2*pi*r,3)
+a=round(pi*r**2,3)
+v=round(4/3*pi*r**3,3)
+
+print()
+print('radious =',r)
+print('diameter =',d)
+print('circumference =',c)
+print('area =',a)
+print('volume =',v)
+
+
